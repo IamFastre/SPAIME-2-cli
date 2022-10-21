da879329f763b7a9fd6808811082576bd9961c2e
IamFastre <mahmoudmode057@gmail.com>
2022-10-21 22:06:49 +0200

    SPAIME-2

--- apps/ttt.py
+++ apps/ttt.py
@@ -1,14 +1,18 @@
 import os, sys
-from secrets import choice
 from os.path import dirname, join, abspath
 from time import sleep
 
 if __name__ == "__main__":
     sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 
+import random, math, re
+
+# Importing my modules
 from res.colors import *
 from res.libs import *
 
+# So we can happily fry your device
+sys.setrecursionlimit(100000)
 
 
 class s():
